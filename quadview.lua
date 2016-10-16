2f8315b5c243a393e6e09d321bf0ea5cd795dc78
zohooo <zohooo@yeah.net>
2016-10-16 12:28:50 +0800

    move config file

--- quadview.lua
+++ quadview.lua
@@ -24,6 +24,7 @@ sep = package.config:sub(1,1) -- path separator
 
 mainpath = wx.wxGetCwd()
 datapath = os.getenv("APPDATA") .. sep .. "QuadView"
+cfgname = datapath .. sep .. "quadview.ini"
 runname = datapath .. sep .. "running"
 
 if not wx.wxFileName.DirExists(datapath) and not wx.wxFileName.Mkdir(datapath) then
@@ -58,7 +59,7 @@ end)
 -----------------------------------------------------------
 
 function GetConfig()
-    local config = wx.wxFileConfig("QuadView")
+    local config = wx.wxFileConfig("QuadView", "", cfgname)
     if config then
         config:SetRecordDefaults()
     else
